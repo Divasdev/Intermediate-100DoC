d7447ed7f6d8c689684c7129d65cbcf092087a98
Divasdev <sharmadivas881@gmail.com>
2025-12-22 11:16:11 +0530

    Starting code for OOP method for the copfee machine

--- day-16.py
+++ day-16.py
@@ -1,3 +1,43 @@
-import turtle
 
-timmy=turtle.Turtle()
+#import another_module
+#print(another_module.another_var)
+
+
+# from turtle import Turtle, Screen
+#
+# timmy=Turtle()
+# print(timmy)
+# timmy.color("red","yellow")
+# timmy.shape("turtle")
+# timmy.forward(100)
+# timmy.right(90)
+#
+#
+#
+#
+# my_screen=Screen()
+# print(my_screen.canvheight)
+#
+# my_screen.exitonclick()
+
+from  prettytable import PrettyTable
+
+
+table = PrettyTable()
+
+
+table.add_column("Pokemon name",["Pikachu","charmander","Squirtle"])
+
+
+table.add_column("Pokemon type",["Electric","water","Fire"])
+
+
+table.align="c"
+
+print(table)
+
+
+
+
+
+
