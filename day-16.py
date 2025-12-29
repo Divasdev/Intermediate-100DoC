ab46a3ba32fce572a2a647e079283b85a72549a4
Divasdev <sharmadivas881@gmail.com>
2025-12-29 09:49:01 +0530

    creating class in OOP

--- day-16.py
+++ day-16.py
@@ -1,22 +1,22 @@
-# class Origin :
-#     def __init__(self,origin):
-#         self.origin=origin
-#
-#
-# class Cars:
-#     def __init__(self,name,price,origin):
-#         self.name = name
-#         self.price = price
-#         self.origin=origin
-#
-#     def __str__(self):
-#         return f"{self.name} | Price:{self.price} | Origin:{self.origin}"
-#
-# origin1=Origin("Stuttgart")
-# c1=Cars("Porsche","2CR", "origin1")
-# c2=Cars("BMW","3CR","Berlin")
-#
-# print(c1)
+class Origin :
+    def __init__(self,origin):
+        self.origin=origin
+
+
+class Cars:
+    def __init__(self,name,price,origin):
+        self.name = name
+        self.price = price
+        self.origin=origin
+
+    def __str__(self):
+        return f"{self.name} | Price:{self.price} | Origin:{self.origin}"
+
+origin1=Origin("Stuttgart")
+c1=Cars("Porsche","2CR", "origin1")
+c2=Cars("BMW","3CR","Berlin")
+
+print(c1)
 
 
 
