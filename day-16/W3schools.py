0486629d1258c8332f37e61829a07d3ee7649065
Divasdev <sharmadivas881@gmail.com>
2026-01-05 20:25:53 +0530

    learning object oriented programming from W3 schools

---
+++ day-16/W3schools.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self,name,age,address):
+        self.name=name
+        self.age=age
+        self.address=address
+    def greet(self):
+        return "Hello," + self.name
+    def welcome(self):
+        message =self.greet()
+        print(message,"! Welcome to our Website.")
+
+
+p1=Person("Divas",20,"Kanglatongbi")
+p1.welcome()
+
+
+p1.name="Archita"
+p1.welcome()
+p1.age=19
+print(p1.age)
