2039e5d6312a82c4f25335bfb35c6a23120c3635
Divasdev <sharmadivas881@gmail.com>
2026-01-17 15:25:18 +0530

    day-22

--- day-22/main.py
+++ day-22/main.py
@@ -1,36 +1,50 @@
-from turtle import Turtle,Screen
+from turtle import Screen
 from paddle import Paddle,Ball
 
-r_paddle=Paddle((350,0),5)
-l_paddle=Paddle((-350,0),5)
+import time
 
-
-
-
-screen=Screen()
+# Screen setup
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
 screen.tracer(0)
+
+# Create paddles
+r_paddle = Paddle((350, 0), 5)
+l_paddle = Paddle((-350, 0), 5)
+
+# Create ball
+ball = Ball()
+
+# Keyboard bindings
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
-screen.setup(800,600)
-screen.bgcolor("black")
-screen.title("pong")
 
-
-ball =Ball()
-game_is_on=True
+# Game loop
+game_is_on = True
 while game_is_on:
-     screen.update()
-     ball.move()
+    time.sleep(0.05)
+    screen.update()
+    ball.move()
 
+    # Wall collision
+    if ball.ball.ycor() > 280 or ball.ball.ycor() < -280:
+        ball.bounce_y()
 
+    # Paddle collision (right)
+    if ball.ball.distance(r_paddle.paddle) < 50 and ball.ball.xcor() > 320:
+        ball.bounce_x()
 
+    # Paddle collision (left)
+    if ball.ball.distance(l_paddle.paddle) < 50 and ball.ball.xcor() < -320:
+        ball.bounce_x()
 
-
-
-
+    # Ball out of bounds
+    if ball.ball.xcor() > 380 or ball.ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
-
--- day-22/paddle.py
+++ day-22/paddle.py
@@ -22,20 +22,28 @@ class Paddle:
 
 class Ball:
     def __init__(self):
-        self.ball=Turtle()
+        self.ball = Turtle()
         self.ball.shape("circle")
         self.ball.color("white")
         self.ball.penup()
-        self.ball.goto(0,0)
-        self.ball.speed("fast")
-        self.x_move=3
-        self.y_move=3
+        self.ball.goto(0, 0)
+
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x=self.ball.xcor() + self.x_move
-        new_y=self.ball.ycor() + self.y_move
-        self.ball.goto(new_x,new_y)
+        new_x = self.ball.xcor() + self.x_move
+        new_y = self.ball.ycor() + self.y_move
+        self.ball.goto(new_x, new_y)
 
+    def bounce_y(self):
+        self.y_move *= -1
 
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.ball.goto(0, 0)
+        self.bounce_x()
 
 
