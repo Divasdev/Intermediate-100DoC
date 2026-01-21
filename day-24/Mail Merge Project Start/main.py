d3f27e711be4c0f9984fe10b90350e5aa90d715b
Divasdev <sharmadivas881@gmail.com>
2026-01-21 09:40:14 +0530

    day-24

--- day-24/Mail Merge Project Start/main.py
+++ day-24/Mail Merge Project Start/main.py
@@ -6,3 +6,16 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Letters/starting_letter.txt") as file:
+    letter_template = file.read()
+
+with open("Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
+for name in names:
+    clean_name = name.strip()
+    personalized_letter = letter_template.replace("[name]", clean_name)
+
+    with open(f"Output/ReadyToSend/letter_for_{clean_name}.txt", "w") as file:
+        file.write(personalized_letter)
--- day-24/main.py
+++
@@ -1,5 +0,0 @@
-with open("/Users/divassharma/PycharmProjects/Intermediate-100DoC/day-24/main.py") as file:
-    contents = file.read()
-    print(contents)
-
-
