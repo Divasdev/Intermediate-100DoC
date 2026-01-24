e3939e027c7af8561cb3ebcc44e305f1d5cea5cf
Divasdev <sharmadivas881@gmail.com>
2026-01-24 08:58:18 +0530

    day-25

--- day-25/main.py
+++ day-25/main.py
@@ -15,18 +15,37 @@ import pandas
 # mon_temp=monday.temp[0]
 # mon_temp_F=mon_temp*9/5+32
 # print(mon_temp_F)
+#
+# data_dict={
+#     "students":["amy","divas","sharma"],
+#     "scores":[76,45,35]
+# }
+#
+#
+# data=pandas.DataFrame(data_dict)
+#
+# data.to_csv("new_data.csv")
+# print(data)
+#
 
-data_dict={
-    "students":["amy","divas","sharma"],
-    "scores":[76,45,35]
+
+data=pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20260124.csv")
+grey=len(data[data["Primary Fur Color"]=="Gray"])
+red=len(data[data["Primary Fur Color"]=="Cinnamon"])
+black=len(data[data["Primary Fur Color"]=="Black"])
+print(grey)
+print(red)
+print(black)
+
+
+data_dit={
+    "Fur Color":["Grey","Cinnamon","Black"],
+
+    "count":[grey,red,black]
+
+
 }
 
-
-data=pandas.DataFrame(data_dict)
-
-data.to_csv("new_data.csv")
-print(data)
-
-
-
+df=pandas.DataFrame(data_dit)
+df.to_csv("sq_count.csv")
 
