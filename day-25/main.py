2977a8bf7f06a0bb4c429d87c65c8cf70dd37288
Divasdev <sharmadivas881@gmail.com>
2026-01-24 08:40:29 +0530

    day-25

--- day-25/main.py
+++ day-25/main.py
@@ -1,5 +1,15 @@
 
 import pandas
 data=pandas.read_csv("weather_data.csv")
-print(data["temp"])
+print(type(data))
+temp_data=data["temp"]
+
+print(temp_data.max())
+
+print(data[data.day=="Monday"])
+print(data[data.temp==data.temp.max()])
+
+
+
+
 
