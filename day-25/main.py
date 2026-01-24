c8ec97a7bfc7a07d88c31e2a190f50a6f1a7ce88
Divasdev <sharmadivas881@gmail.com>
2026-01-24 08:47:38 +0530

    day-25

--- day-25/main.py
+++ day-25/main.py
@@ -1,14 +1,31 @@
 
 import pandas
-data=pandas.read_csv("weather_data.csv")
-print(type(data))
-temp_data=data["temp"]
+# data=pandas.read_csv("weather_data.csv")
+# print(type(data))
+# temp_data=data["temp"]
+#
+# print(temp_data.max())
+#
+# print(data[data.day=="Monday"])
+# print(data[data.temp==data.temp.max()])
+#
+#
+# monday=data[data.day=="Monday"]
+# print(monday.condition)
+# mon_temp=monday.temp[0]
+# mon_temp_F=mon_temp*9/5+32
+# print(mon_temp_F)
 
-print(temp_data.max())
+data_dict={
+    "students":["amy","divas","sharma"],
+    "scores":[76,45,35]
+}
 
-print(data[data.day=="Monday"])
-print(data[data.temp==data.temp.max()])
 
+data=pandas.DataFrame(data_dict)
+
+data.to_csv("new_data.csv")
+print(data)
 
 
 
