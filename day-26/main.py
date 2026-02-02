2c93b526a90874088f2aefdc7db9ab4196489c2d
Divasdev <sharmadivas881@gmail.com>
2026-02-02 08:26:02 +0530

    day-26 list comprehension

--- day-26/main.py
+++ day-26/main.py
@@ -1,1 +1,21 @@
-#List Comprehension
+#new_dict={new_key:new_value for (key,value) in dict.items()}
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# list_of_words=sentence.split()
+#
+# result={word:len(word) for word  in list_of_words}
+# print(result)
+
+
+# weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+#
+# weather_f = {day_name:((temp_c* 9/5)+32) for (day_name,temp_c) in weather_c.items()}
+#
+# print(weather_f)
+
+
+
+student_dict={
+    "student":["Divas","Tushar"],
+    "score":[56,78]
+}
+
