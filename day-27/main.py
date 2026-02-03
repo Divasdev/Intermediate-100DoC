45967946fa0733a6773a70727cae6200896190fc
Divasdev <sharmadivas881@gmail.com>
2026-02-03 11:09:22 +0530

    day-27 Graphical User Interface

--- day-27/main.py
+++ day-27/main.py
@@ -6,7 +6,7 @@ window.title("My first GUI program")
 window.minsize(500,300)
 
 my_label=tkinter.Label(text="I am a label",font=("Sans Serif",24,"italic"))
-my_label.pack()
+my_label.pack(side="left")
 
 
 
