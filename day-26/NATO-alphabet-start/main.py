1cf03f45ad5aed752998f21a192009d49d515a25
Divasdev <sharmadivas881@gmail.com>
2026-02-20 14:09:43 +0530

    day-30 errors and exceptions

--- day-26/NATO-alphabet-start/main.py
+++ day-26/NATO-alphabet-start/main.py
@@ -1,20 +1,20 @@
 import pandas
-
-
-#TODO 1. Create a dictionary in this format:
-
 alphabet_dataframe=pandas.read_csv("nato_phonetic_alphabet.csv")
-print(alphabet_dataframe)
+
 
 alphabet_dict={row.letter:row.code
                for (index,row) in alphabet_dataframe.iterrows()
                }
-
 print(alphabet_dict)
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_word = input("Enter a word: ").upper()
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
+    try:
+        phonetic_list = [alphabet_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry Only Alphabet letter")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
 
-phonetic_list = [alphabet_dict[letter] for letter in user_word]
-
-print(phonetic_list)
+generate_phonetic()
--- day-30/main.py
+++ day-30/main.py
@@ -1,15 +1,36 @@
+#
+#
+#
+# # Catch the exception and make sure the code runs without crashing.
+# fruits = ["Apple", "Pear", "Orange"]
+#
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#         print(fruit + " pie")
+#     except IndexError:
+#         print("Fruit pie")
+#
+# make_pie(4)
+#
 
 
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
 
-# Catch the exception and make sure the code runs without crashing.
-fruits = ["Apple", "Pear", "Orange"]
 
-def make_pie(index):
-    try:
-        fruit = fruits[index]
-        print(fruit + " pie")
-    except IndexError:
-        print("Fruit pie")
+def count_likes(posts):
+    total_likes = 0
+    for post in posts:
+        total_likes = total_likes + post.get('Likes',0)
+    return total_likes
 
-make_pie(4)
+
+count_likes(facebook_posts)
 
