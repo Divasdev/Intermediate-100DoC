a7e3094433c32835a29c1575d788d01ce4643b8f
Divasdev <sharmadivas881@gmail.com>
2026-03-11 20:35:40 +0530

    day-33

---
+++ day-34/quizzler-app-start/data.py
@@ -0,0 +1,16 @@
+import requests
+API_URL="https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean"
+
+response=requests.get(API_URL)
+data=response.json()
+
+question_data = {
+    "category": question["category"],
+    "type": question["type"],
+    "difficulty": question["difficulty"],
+    "question": question["question"],
+    "correct_answer": question["correct_answer"],
+    "incorrect_answers": question["incorrect_answers"]
+}
+
+
